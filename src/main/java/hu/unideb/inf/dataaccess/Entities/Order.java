cef8b0b05a5a6b96f65bdc47d89aa1f53346bb10
csabad1997 <csabad1997@gmail.com>
2021-04-18 17:40:50 +0200

    Order OrderState property értékadás javítás

--- src/main/java/hu/unideb/inf/dataaccess/Entities/Order.java
+++ src/main/java/hu/unideb/inf/dataaccess/Entities/Order.java
@@ -1,5 +1,6 @@
 package hu.unideb.inf.dataaccess.Entities;
 
+import hu.unideb.inf.businesslogic.Enums.OrderState;
 import hu.unideb.inf.dataaccess.IEntity;
 
 import java.sql.Date;
@@ -10,14 +11,14 @@ public class Order implements IEntity<Order> {
     public int Id;
     public String Name;
     public Date Date;
-    public int State;
+    public OrderState State;
     @Override
     public Order Map(ResultSet rs) {
         try {
             Id = rs.getInt("C_ID");
             Name = rs.getString("C_NAME");
             Date = Date.valueOf(rs.getString("C_DATE"));
-            State = rs.getInt("C_STATE");
+            State = OrderState.fromInt(rs.getInt("C_STATE"));
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
