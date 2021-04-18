2cda014383c7edaa0ef6f8425c497530821d0a59
csabad1997 <csabad1997@gmail.com>
2021-04-18 17:41:26 +0200

    Enum típusok javítás

--- src/main/java/hu/unideb/inf/businesslogic/Enums/ItemType.java
+++ src/main/java/hu/unideb/inf/businesslogic/Enums/ItemType.java
@@ -1,21 +1,38 @@
 package hu.unideb.inf.businesslogic.Enums;
 
 public enum ItemType {
+    NotSet,
     Base,
     Topping,
     Sauce;
     public static ItemType fromInt(int i){
         switch(i){
-            case 0:{
+            case 1:{
                 return Base;
             }
-            case 1:{
+            case 2:{
                 return Topping;
             }
-            case 2:{
+            case 3:{
                 return Sauce;
             }
         }
-        return null;
+        return NotSet;
+    }
+    public static int toInt(ItemType val){
+        switch(val){
+            case Base:{
+                return 1;
+            }
+            case Topping:{
+                return 2;
+            }
+            case Sauce:{
+                return 3;
+            }
+            default:{
+                return 0;
+            }
+        }
     }
 }
