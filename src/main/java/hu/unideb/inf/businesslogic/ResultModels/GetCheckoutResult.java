b7f8dda38a3ce9b3c2fc7baa63bfde8cf74d2288
csabad1997 <csabad1997@gmail.com>
2021-04-21 18:11:56 +0200

    Resultmodellek megvalósítása

--- src/main/java/hu/unideb/inf/businesslogic/ResultModels/GetCheckoutResult.java
+++ src/main/java/hu/unideb/inf/businesslogic/ResultModels/GetCheckoutResult.java
@@ -1,4 +1,9 @@
 package hu.unideb.inf.businesslogic.ResultModels;
 
 public class GetCheckoutResult {
+    public int Sum;
+    public String Name;
+    public GetCheckoutResult(int sum){
+        this.Sum = sum;
+    }
 }
